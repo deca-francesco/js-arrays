9a3f40043080bcbee5a875a57be4b446a9fde315
deca-francesco <francescodecarlo70@gmail.com>
2024-10-02 16:00:10 +0200

    ADD: exercise 02 bonus 2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -19,6 +19,14 @@ console.log(exercise, reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+exercise = "Esercizio Bonus 2 :";
+const longNames = [];
+for (let i = 0; i < teachers.length; i++) {
+  const teacher = teachers[i];
+  if (teacher.length >= 5) {
+    longNames.push(teacher);
+  }
+}
+console.log(exercise, longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
